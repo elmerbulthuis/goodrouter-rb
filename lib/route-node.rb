96cadeeeb70c84908d810919229002788032d72b
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-05-24 14:25:53 +0200

    empty files

---
+++ lib/route-node.rb
@@ -0,0 +1,3 @@
+class RouteNode
+    
+end
---
+++ lib/template.rb
@@ -0,0 +1,13 @@
+def parse_template_parts(
+    route_template,
+    parameter_placeholder_re,
+)
+
+end
+
+def parse_template_pairs(
+    route_template,
+    parameter_placeholder_re,
+)
+
+end
