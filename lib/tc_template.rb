60ff7bd420e9e7f54be52d38cf4660fe650532a9
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-05-30 11:55:42 +0200

    wip

--- lib/tc_template.rb
+++ lib/tc_template.rb
@@ -2,13 +2,13 @@ require_relative "template"
 require "test/unit"
 
 class TestTemplate < Test::Unit::TestCase
- 
+
     def parse_template_parts
         assert_equal(1, 1)
       end
-    
+
       def parse_template_pairs
         assert_equal(1, 1)
       end
-        
+
 end
